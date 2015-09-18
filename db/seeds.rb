add56b4cc78597c12adb6efe42b9897704f23d21
Jasmine Feldmann <jasmine.feldmann@gmail.com>
2015-09-18 09:12:17 -0500

    Add nokogiri gem

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,6 +3,7 @@
 #find sibling of .list_head elements
 
 #https://blog.hartleybrody.com/web-scraping/
+#http://ruby.bastardsbook.com/chapters/html-parsing/
 
 #iterate over letter=a&i=2
 #a=17, b=37, c=35, d=13, e=5, f=18, g=17 , h=13 , i=6 , j=8 , k=7 , l=13 , m=21 , n=7 , o=6 , p=18 , q=1 , r=12 , s=25 , t=16 , u=1 , v=4 , w=7 , x=1 , y=2 , z=1
