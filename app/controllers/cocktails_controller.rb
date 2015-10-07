df74f3dde82fe8bdc95037d40cdcef8336045002
Jasmine Feldmann <jasmine.feldmann@gmail.com>
2015-10-07 14:34:33 -0500

    Add working search for multiple

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -1,12 +1,16 @@
 class CocktailsController < ApplicationController
 
 def index
-    if params[:query].present?
-      @ingredients = Ingredient.search(params[:query], name: params[:name], order: {name: :asc})
-      @cocktails = @ingredients.map {|ingredient| ingredient.cocktails}.flatten!
-    else
-      @cocktails = Cocktail.all
-    end
+  if params[:query1].present?
+    @ingredients << Ingredient.search(params[:query1], name: params[:name], order: {name: :asc}) 
+    @ingredients << Ingredient.search(params[:query2], name: params[:name], order: {name: :asc}) 
+    @ingredients.flatten!
+    cocktails = @ingredients.map {|ingredient| ingredient.cocktails}.flatten! 
+    @ingredients.each do |c| puts c.name end
+    @cocktails = cocktails.select { |cocktail| cocktails.count(cocktail) > 1}.uniq
+  else
+    @cocktails = Cocktail.all
   end
+end
 
 end
