cfb67a4a9b66a5b53c27bf5c809aa0c571988c2b
Jasmine Feldmann <jasmine.feldmann@gmail.com>
2015-10-07 16:42:28 -0500

    add working search for up to four ingredients

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -3,7 +3,59 @@ class CocktailsController < ApplicationController
 def index
   @cocktails = []
   hash = {}
-  if params[:query2].present?
+  if params[:query4].present?
+  	temp1 = {}
+  	temp2 = {}
+    ingredients1 = Ingredient.search(params[:query1], name: params[:name], order: {name: :asc}) 
+    ingredients2 = Ingredient.search(params[:query2], name: params[:name], order: {name: :asc}) 
+    ingredients3 = Ingredient.search(params[:query3], name: params[:name], order: {name: :asc}) 
+    ingredients4 = Ingredient.search(params[:query3], name: params[:name], order: {name: :asc}) 
+    cocktails = ingredients1.map {|ingredient| ingredient.cocktails}.flatten! 
+    new_cocktails = ingredients2.map {|ingredient| ingredient.cocktails}.flatten! 
+    cocktails_3 = ingredients3.map {|ingredient| ingredient.cocktails}.flatten! 
+    cocktails_4 = ingredients4.map {|ingredient| ingredient.cocktails}.flatten! 
+   	cocktails.each do |i| 
+   		hash[i] = 1
+   	end
+   	new_cocktails.each do |j|
+   		if hash.has_key?(j)
+   			temp1[j] = 1
+   			cocktails_3.each do |k|
+   				if temp1.has_key?(k)
+   					temp2[k] = 1
+   					cocktails_4.each do |l|
+   						if temp2.has_key?(l)
+   							@cocktails << l
+   						end
+   					end
+   				end
+   			end
+   		end
+   	end
+   	@cocktails.uniq!
+  elsif params[:query3].present?
+  	temp = {}
+    ingredients1 = Ingredient.search(params[:query1], name: params[:name], order: {name: :asc}) 
+    ingredients2 = Ingredient.search(params[:query2], name: params[:name], order: {name: :asc}) 
+    ingredients3 = Ingredient.search(params[:query3], name: params[:name], order: {name: :asc}) 
+    cocktails = ingredients1.map {|ingredient| ingredient.cocktails}.flatten! 
+    new_cocktails = ingredients2.map {|ingredient| ingredient.cocktails}.flatten! 
+    cocktails_3 = ingredients3.map {|ingredient| ingredient.cocktails}.flatten! 
+   	cocktails.each do |i| 
+   		hash[i] = 1
+   	end
+   	new_cocktails.each do |j|
+   		if hash.has_key?(j)
+   			temp[j] = 1
+   			cocktails_3.each do |k|
+   				if temp.has_key?(k)
+   					@cocktails << k
+   				end
+   			end
+   		end
+   	end
+   	@cocktails.uniq!
+  elsif params[:query2].present?
     ingredients1 = Ingredient.search(params[:query1], name: params[:name], order: {name: :asc}) 
     ingredients2 = Ingredient.search(params[:query2], name: params[:name], order: {name: :asc}) 
     cocktails = ingredients1.map {|ingredient| ingredient.cocktails}.flatten! 
