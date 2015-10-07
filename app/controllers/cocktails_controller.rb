639fb49e51d0c9b8660178c0e961fa0b0e6b1de4
Jasmine Feldmann <jasmine.feldmann@gmail.com>
2015-10-06 19:40:41 -0500

    update search gem

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -1,6 +1,12 @@
 class CocktailsController < ApplicationController
 
-  def index
+def index
+    if params[:query].present?
+      @ingredients = Ingredient.search(params[:query], name: params[:name])
+      @cocktails = @ingredients.map {|ingredient| ingredient.cocktails}.flatten!
+    else
+      @cocktails = Cocktail.all
+    end
   end
 
 end
