af1d800b7a886b8b80abfc85e1cbb4a5d880d95f
Jasmine Feldmann <jasmine.feldmann@gmail.com>
2015-10-07 14:45:45 -0500

    Add working for two params

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -1,13 +1,24 @@
 class CocktailsController < ApplicationController
 
 def index
-  if params[:query1].present?
-    @ingredients << Ingredient.search(params[:query1], name: params[:name], order: {name: :asc}) 
-    @ingredients << Ingredient.search(params[:query2], name: params[:name], order: {name: :asc}) 
-    @ingredients.flatten!
-    cocktails = @ingredients.map {|ingredient| ingredient.cocktails}.flatten! 
-    @ingredients.each do |c| puts c.name end
-    @cocktails = cocktails.select { |cocktail| cocktails.count(cocktail) > 1}.uniq
+  @cocktails = []
+  hash = {}
+  if params[:query2].present?
+    ingredients1 = Ingredient.search(params[:query1], name: params[:name], order: {name: :asc}) 
+    ingredients2 = Ingredient.search(params[:query2], name: params[:name], order: {name: :asc}) 
+    cocktails = ingredients1.map {|ingredient| ingredient.cocktails}.flatten! 
+    new_cocktails = ingredients2.map {|ingredient| ingredient.cocktails}.flatten! 
+   	cocktails.each do |i| 
+   		hash[i] = 1
+   	end
+   	new_cocktails.each do |j|
+   		if hash.has_key?(j)
+   			@cocktails << j
+   		end
+   	end
+  elsif params[:query1].present?
+  	@ingredients << Ingredient.search(params[:query1], name: params[:name], order: {name: :asc})
+    @cocktails = @ingredients.map {|ingredient| ingredient.cocktails}
   else
     @cocktails = Cocktail.all
   end
