bb3e747c1efcf5726ade60b0b18073a79b0875be
Jasmine Feldmann <jasmine.feldmann@gmail.com>
2015-10-07 12:30:28 -0500

    Update results to be links to external website

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -2,7 +2,7 @@ class CocktailsController < ApplicationController
 
 def index
     if params[:query].present?
-      @ingredients = Ingredient.search(params[:query], name: params[:name])
+      @ingredients = Ingredient.search(params[:query], name: params[:name], order: {name: :asc})
       @cocktails = @ingredients.map {|ingredient| ingredient.cocktails}.flatten!
     else
       @cocktails = Cocktail.all
