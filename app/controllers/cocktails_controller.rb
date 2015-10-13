82b9dbc2337c1b0a85b30f01a3b3212c8edda4e1
Jasmine Feldmann <jasmine.feldmann@gmail.com>
2015-10-13 13:54:06 -0500

    Add working search functionality

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -17,31 +17,52 @@ class CocktailsController < ApplicationController
   end
 
   def show
-    hash = {}
-    @cocktails = []
-    @ingredients = Ingredient.all.map {|ingredient| ingredient}
-    @ingredients.select! {|ingredient| ingredient.name.downcase.include?(params[:term])}
-    if params[:ingredients] 
-      old_ingredients = []
+    possible_cocktails = Hash.new(0)
+    @ingredients = []
+    @ingredients << Ingredient.find_by(name: params[:term])
+    if params[:ingredients]
       params[:ingredients].each do |ing|
-        old_ingredients << Ingredient.find_by(name: ing)
+        @ingredients << Ingredient.find_by(name: ing)
       end
-      cocktails = @ingredients.map {|ingredient| ingredient.cocktails }.flatten
-      old_cocktails = old_ingredients.map {|ingredient| @cocktails << ingredient.cocktails }.flatten!
-      old_cocktails.each do |cocktail|
-        hash[cocktail] = 1
-      end
-      cocktails.each do |cocktail|
-        if hash.has_key?(cocktail)
-          @cocktails << cocktail
-        end
-      end
-      @cocktails = @cocktails.uniq.flatten
-    else
-      @cocktails = @ingredients.map {|ingredient| ingredient.cocktails }.flatten
     end
-
+    @ingredients.each do |ingredient|
+      cocktails = ingredient.cocktails
+      cocktails.each do |cocktail|
+        possible_cocktails[cocktail] += 1
+      end
+    end
+    length = @ingredients.length
+    @cocktails = possible_cocktails.map{ |k,v| v == length ? k : nil }.compact
   end
+
+  # def show
+  #   hash = {}
+  #   @cocktails = []
+  #   @ingredients = Ingredient.all.map {|ingredient| ingredient}
+  #   @ingredients.select! {|ingredient| ingredient.name.downcase.include?(params[:term])}
+  #   if params[:ingredients] 
+  #     old_ingredients = []
+  #     params[:ingredients].each do |ing|
+  #       old_ingredients << Ingredient.find_by(name: ing)
+  #     end
+  #     cocktails = @ingredients.map {|ingredient| ingredient.cocktails }.flatten
+  #     old_cocktails = old_ingredients.map {|ingredient| @cocktails << ingredient.cocktails }.flatten!
+  #     old_cocktails.each do |cocktail|
+  #       hash[cocktail] = 1
+  #     end
+  #     cocktails.each do |cocktail|
+  #       if hash.has_key?(cocktail)
+  #         @cocktails << cocktail
+  #       end
+  #     end
+  #     @cocktails = @cocktails.uniq.flatten
+  #   else
+  #     @cocktails = @ingredients.map {|ingredient| ingredient.cocktails }.flatten
+  #   end
+
+  # end
+
+
 
 
 end
